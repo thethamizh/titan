f093b4802cd59366852377f1c76da84c7e4c1d75
thethamizh <thamizhtds@gmail.com>
2023-06-19 15:35:45 +0530

    Updated with QuerySElector

--- dom.js
+++ dom.js
@@ -1,12 +1,20 @@
-let header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
-let items=document.getElementsByClassName('list-group-item');
-for (let i=0; i<items.length; i++){
-    items[i].style.backgroundColor = "#f4f4f4";
-    items[i].style.fontWeight= "bold";
+// let header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
+// let items=document.getElementsByClassName('list-group-item');
+// for (let i=0; i<items.length; i++){
+//     items[i].style.backgroundColor = "#f4f4f4";
+//     items[i].style.fontWeight= "bold";
+// }
+// items[2].style.backgroundColor= "green";
+// let newitem=document.getElementsByClassName('newitem');
+// newitem[0].style.fontWeight = "bold";
+// let tagitem=document.getElementsByTagName('li');
+// tagitem[4].style.border = 'solid 4px #000';
+
+// QuerySelector //
+let bolditem = document.querySelectorAll('.list-group-item');
+for (i=0; i<bolditem.length; i++){
+    bolditem[i].style.fontWeight='bold';
 }
-items[2].style.backgroundColor= "green";
-let newitem=document.getElementsByClassName('newitem');
-newitem[0].style.fontWeight = "bold";
-let tagitem=document.getElementsByTagName('li');
-tagitem[4].style.border = 'solid 4px #000';
+bolditem[1].style.backgroundColor='green';
+bolditem[2].style.display='None';
