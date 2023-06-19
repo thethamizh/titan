2ab0851db02a4944dae6d3705948e029dc8c0c5b
thethamizh <thamizhtds@gmail.com>
2023-06-19 10:24:13 +0530

    Add dom.js

---
+++ dom.js
@@ -0,0 +1,6 @@
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+let items=document.getElementsByClassName('list-group-item');
+for (let i=0; i<items.length; i++){
+    items[i].style.backgroundColor= "#f4f4f4";
+}
