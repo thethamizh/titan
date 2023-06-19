07e6664857d430090d5e1c1e1bfa3cc8f9221b19
thethamizh <thamizhtds@gmail.com>
2023-06-19 10:41:08 +0530

    Update dom with new changes

--- dom.js
+++ dom.js
@@ -3,4 +3,6 @@ header.style.borderBottom = 'solid 3px #000';
 let items=document.getElementsByClassName('list-group-item');
 for (let i=0; i<items.length; i++){
     items[i].style.backgroundColor= "#f4f4f4";
+    items[i].style.fontWeight= "bold";
 }
+items[2].style.backgroundColor= "green";
