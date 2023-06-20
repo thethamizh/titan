68c3939cc685e35d53adf95d0aee1e924853f5fc
thethamizh <thamizhtds@gmail.com>
2023-06-20 19:10:47 +0530

    Filter Feature Added

--- dom.js
+++ dom.js
@@ -1,16 +1,22 @@
 let form = document.getElementById('addForm');
 let itemlist = document.getElementById('items');
-
+var filter = document.getElementById('filter');
 // form submit event
 form.addEventListener('submit', additem);
 itemlist.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
+
 
 function additem(e){
     e.preventDefault();
     let newitem=document.getElementById('item').value;
+    let des=document.getElementById('des').value;
+
     let li = document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(newitem));
+    li.appendChild(document.createTextNode(des));
+
     let dbtn=document.createElement('button');
     let ebtn=document.createElement('button');
     dbtn.className='btn btn-danger btn-sm float-right delete';
@@ -28,6 +34,25 @@ function removeItem(e){
       if(confirm('Do you want to this Item ?')){
         var li = e.target.parentElement;
         itemlist.removeChild(li);
+      }
+    }
+  }
+  function filterItems() {
+    var input = document.getElementById('filter');
+  var filter = input.value.toUpperCase();
+    var itemList = document.getElementById('items');
+    var items = itemList.getElementsByTagName('li');
+  
+    // Loop through all list items
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      var text = item.textContent || item.innerText;
+  
+      // Check if the item matches the search string
+      if (text.toUpperCase().indexOf(filter) > -1) {
+        item.style.display = '';
+      } else {
+        item.style.display = 'none';
       }
     }
   }
