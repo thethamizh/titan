4b93dd2bea6e22c28eca7a2290f02220dd6880eb
thethamizh <thamizhtds@gmail.com>
2023-06-21 20:12:22 +0530

    Delete Button Added

--- newdir/ls.js
+++ newdir/ls.js
@@ -13,13 +13,25 @@ function saveUserDetails(event) {
 
   // Create an object to hold the user details
   let userDetails = {
-    name: name,
-    email: email
+     name, email
   };
 
   // Store the user details in local storage with the name as the key
   localStorage.setItem(email, JSON.stringify(userDetails));
+  let userDetailsContainer = document.getElementById('userform');
+  let newli = document.createElement('li');
+  newli.textContent = userDetails.name + ' - ' + userDetails.email;
+  let dbtn=document.createElement('input');
+  dbtn.type='button';
+  dbtn.value='Delete';
+  dbtn.onclick = () => {
+    localStorage.removeItem(userDetails.email)
+    userDetailsContainer.removeChild(newli);
+  }
+  newli.appendChild(dbtn);
+  userDetailsContainer.appendChild(newli);
 
   // Reset the form fields
   form.reset();
+  
 }
